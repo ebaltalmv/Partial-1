eff85242a240d2e63b072c02737359727dfd45de
Emmanuel <emma.balta09@gmail.com>
2026-02-14 12:09:25 -0600

    Add project model

---
+++ Parcial1_Caifanes/Parcial1_Caifanes/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Parcial1_Caifanes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Parcial1_Caifanes/Parcial1_Caifanes/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Parcial1_Caifanes
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
