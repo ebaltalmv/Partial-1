8b2f5452f49a6f353e968fd6e7b021f03cdae0a4
Emmanuel <emma.balta09@gmail.com>
2026-02-16 00:26:10 -0600

    Add new model for concerts

---
+++ Parcial1_Caifanes/Parcial1_Caifanes/Models/ConcertModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parcial1_Caifanes.Models
+{
+    public class ConcertModel
+    {
+        public string ImageUrl { get; set; } = "concert_logo.png";
+        public string Name { get; set; } = string.Empty;
+        public string Date { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string status { get; set; } = string.Empty;
+    }
+}
